55b14704114a49d5b3e3ce942ff128a2f715da42
UnpaidMC <michael.roldan@sansano.usm.cl>
2022-07-07 17:24:54 -0400

    REACT y ejercicios

---
+++ Front End/Mi_proyecto/aplicacion_ejercitacion/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const App = () => {
+    return (
+    <div className = "container mt-4">
+        <div className="row">
+        <div className="col-8 bg-warning">
+            <h1>Usuarios</h1>
+        </div>
+        <div className="col bg-danger">
+            <h1>Cursos</h1>
+        </div>
+        </div>
+    </div>
+    )
+
+};
+
+export default App;
---
+++ Front End/REACT2/aplicacion1/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const App = () => {
+    return (
+    <div className = "container mt-4">
+        <div className="row">
+        <div className="col-8">
+            <h1>Lista</h1>
+        </div>
+        <div className="col">
+            <h1>Formulario</h1>
+        </div>
+        </div>
+    </div>
+    )
+
+};
+
+export default App;
